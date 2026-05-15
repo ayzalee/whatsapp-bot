b149287c621d154d7d678e83907bad7b5de8cd67
ayzalee <ayazaliofc@gmail.com>
2026-05-15 14:21:31 +0500

    remove nested whatsapp-bot folder

--- whatsapp-bot/store/sqlstore/upgrades/05-account-jid-format.sql
+++
@@ -1,2 +0,0 @@
--- v5: Update account JID format
-UPDATE device SET jid=REPLACE(jid, '.0', '');
--- whatsapp-bot/store/sqlstore/upgrades/06-facebook-uuid.sql
+++
@@ -1,2 +0,0 @@
--- v6: Add facebook_uuid column to device table
-ALTER TABLE device ADD COLUMN facebook_uuid uuid;
