b29fbde6bcf1712583ca1b15ae6865bbebb93731
ayzalee <ayazaliofc@gmail.com>
2026-05-22 13:58:58 +0500

    fix

---
+++ plugins/msgdump.go
@@ -0,0 +1,35 @@
+package plugins
+
+import (
+"encoding/json"
+"fmt"
+)
+
+func init() {
+Register(&Command{
+Pattern:  "jsmsg",
+Aliases:  []string{"dump"},
+IsSudo:   true,
+Category: "owner",
+Func: func(ctx *Context) error {
+quoted := quotedMsg(ctx)
+var target interface{}
+if quoted != nil {
+target = quoted
+} else {
+target = ctx.Event.Message
+}
+out, err := json.MarshalIndent(target, "", "  ")
+if err != nil {
+ctx.Reply(fmt.Sprintf("Error: %s", err.Error()))
+return nil
+}
+result := string(out)
+if len(result) > 3500 {
+result = result[:3500] + "\n...(truncated)"
+}
+ctx.Reply("```" + result + "```")
+return nil
+},
+})
+}
