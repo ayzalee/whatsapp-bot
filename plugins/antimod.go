ceeaeb7cd9caaa7cdfe73f51e806bca94d769740
ayzalee <ayazaliofc@gmail.com>
2026-05-31 12:17:42 +0500

    fix pdm and antimod

--- plugins/antimod.go
+++ plugins/antimod.go
@@ -41,91 +41,7 @@ ctx.Reply(fmt.Sprintf(T().AntiModStatus, status))
 return nil
 },
 })
-}
 
-func HandleGroupParticipantChange(client *whatsmeow.Client, evt *events.GroupInfo) {
-chatJID := evt.JID.String()
-
-if !antiModChats[chatJID] {
-return
-}
-
-botUser := client.Store.ID.User
-actor := evt.Sender
-
-// Ignore bot's own actions
-if actor.User == botUser {
-return
-}
-
-// Ignore sudo users
-if BotSettings.IsSudo(actor.User) {
-return
-}
-
-actorJIDStr := actor.ToNonAD().String()
-var mentions []string
-mentions = append(mentions, actorJIDStr)
-
-if len(evt.Demote) > 0 {
-// Re-promote demoted admins
-var promoted []string
-for _, jid := range evt.Demote {
-if jid.User == botUser {
-continue
-}
-client.UpdateGroupParticipants(context.Background(), evt.JID,
-[]types.JID{jid}, whatsmeow.ParticipantChangePromote)
-promoted = append(promoted, jid.User)
-mentions = append(mentions, jid.ToNonAD().String())
-}
-
-// Demote the actor
-client.UpdateGroupParticipants(context.Background(), evt.JID,
-[]types.JID{actor.ToNonAD()}, whatsmeow.ParticipantChangeDemote)
-
-msg := fmt.Sprintf(T().AntiModDemote, actor.User)
-sendMentionMsg(client, evt.JID, msg, mentions)
-}
-
-if len(evt.Promote) > 0 {
-// De-promote promoted users
-for _, jid := range evt.Promote {
-if jid.User == botUser {
-continue
-}
-client.UpdateGroupParticipants(context.Background(), evt.JID,
-[]types.JID{jid}, whatsmeow.ParticipantChangeDemote)
-mentions = append(mentions, jid.ToNonAD().String())
-}
-
-// Demote the actor
-client.UpdateGroupParticipants(context.Background(), evt.JID,
-[]types.JID{actor.ToNonAD()}, whatsmeow.ParticipantChangeDemote)
-
-msg := fmt.Sprintf(T().AntiModPromote, actor.User)
-sendMentionMsg(client, evt.JID, msg, mentions)
-}
-}
-
-func sendMentionMsg(client *whatsmeow.Client, chat types.JID, text string, mentions []string) {
-id := client.GenerateMessageID()
-sendQueue <- sendTask{
-client: client,
-to:     chat,
-msg: &waProto.Message{
-ExtendedTextMessage: &waProto.ExtendedTextMessage{
-Text: proto.String(text),
-ContextInfo: &waProto.ContextInfo{
-MentionedJID: mentions,
-},
-},
-},
-id: id,
-}
-}
-
-func init() {
 Register(&Command{
 Pattern:  "pdm",
 IsGroup:  true,
@@ -151,4 +67,88 @@ ctx.Reply("*PDM Notifications*\nStatus: " + status + "\n\n.pdm on\n.pdm off")
 return nil
 },
 })
+}
+
+func HandleGroupParticipantChange(client *whatsmeow.Client, evt *events.GroupInfo) {
+chatJID := evt.JID.String()
+botUser := client.Store.ID.User
+actor := evt.Sender
+
+if actor.User == botUser {
+return
+}
+
+actorJIDStr := actor.ToNonAD().String()
+actorStr := actor.User
+
+// PDM notifications
+if pdmChats[chatJID] {
+for _, jid := range evt.Promote {
+msg := fmt.Sprintf("_@%s promoted @%s_", actorStr, jid.User)
+sendMentionMsg(client, evt.JID, msg, []string{actorJIDStr, jid.ToNonAD().String()})
+}
+for _, jid := range evt.Demote {
+msg := fmt.Sprintf("_@%s demoted @%s_", actorStr, jid.User)
+sendMentionMsg(client, evt.JID, msg, []string{actorJIDStr, jid.ToNonAD().String()})
+}
+}
+
+// Antimod protection
+if !antiModChats[chatJID] {
+return
+}
+
+if BotSettings.IsSudo(actor.User) {
+return
+}
+
+var mentions []string
+mentions = append(mentions, actorJIDStr)
+
+if len(evt.Demote) > 0 {
+for _, jid := range evt.Demote {
+if jid.User == botUser {
+continue
+}
+client.UpdateGroupParticipants(context.Background(), evt.JID,
+[]types.JID{jid}, whatsmeow.ParticipantChangePromote)
+mentions = append(mentions, jid.ToNonAD().String())
+}
+client.UpdateGroupParticipants(context.Background(), evt.JID,
+[]types.JID{actor.ToNonAD()}, whatsmeow.ParticipantChangeDemote)
+msg := fmt.Sprintf(T().AntiModDemote, actorStr)
+sendMentionMsg(client, evt.JID, msg, mentions)
+}
+
+if len(evt.Promote) > 0 {
+for _, jid := range evt.Promote {
+if jid.User == botUser {
+continue
+}
+client.UpdateGroupParticipants(context.Background(), evt.JID,
+[]types.JID{jid}, whatsmeow.ParticipantChangeDemote)
+mentions = append(mentions, jid.ToNonAD().String())
+}
+client.UpdateGroupParticipants(context.Background(), evt.JID,
+[]types.JID{actor.ToNonAD()}, whatsmeow.ParticipantChangeDemote)
+msg := fmt.Sprintf(T().AntiModPromote, actorStr)
+sendMentionMsg(client, evt.JID, msg, mentions)
+}
+}
+
+func sendMentionMsg(client *whatsmeow.Client, chat types.JID, text string, mentions []string) {
+id := client.GenerateMessageID()
+sendQueue <- sendTask{
+client: client,
+to:     chat,
+msg: &waProto.Message{
+ExtendedTextMessage: &waProto.ExtendedTextMessage{
+Text: proto.String(text),
+ContextInfo: &waProto.ContextInfo{
+MentionedJID: mentions,
+},
+},
+},
+id: id,
+}
 }
